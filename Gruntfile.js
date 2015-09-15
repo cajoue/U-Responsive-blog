e129ffa1fbf1120640512e618ad4893703b5164a
cajoue <cajoue@users.noreply.github.com>
2015-09-15 09:33:38 +0200

    what was my final version slightly mangled by trying to put the solution answers into my code - will fix later

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,35 @@
 /*
- After you have changed the settings at "Your code goes here",
+ After you have changed the settings under responsive_images
  run this with one of these options:
   "grunt" alone creates a new, completed images directory
   "grunt clean" removes the images directory
   "grunt responsive_images" re-processes images without removing the old ones
-  */
-  "use strict"
+*/
 
-  var ngrok = require("ngrok");
+module.exports = function(grunt) {
 
-  module.exports = function(grunt) {
-    /* Load grunt tasks */
-    require("load-grunt-tasks") (grunt);
-
-    grunt.initConfig({
-      responsive_images: {
-        dev: {
-          options: {
-            engine: "im",
-            sizes: [{
-            /*
-            Change these:
-            */
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            /* Change these: */
             name: "small",
             width: 320,
             quality: 80
             /* 30 and 60 are a bit rubbish */
           },{
-            name: "large",
-            width: 640,
+            name: "medium",
+            width: 500,
+            quality: 80
+          },{
+            name: "large_1x",
+            width: 800,
             quality: 80
           },{
             width: 1600,
-            suffix: "_large_2x",
+            name: "large_2x",
             quality: 30
           }]
         },
@@ -43,9 +40,9 @@
         */
         files: [{
           expand: true,
-          src: ["*.{gif,jpg,png}"],
-          cwd: "images_src/",
-          dest: "images/"
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
         }]
       }
     },
@@ -53,7 +50,7 @@
     /* Clear out the images directory if it exists */
     clean: {
       dev: {
-        src: ["images"],
+        src: ['images'],
       },
     },
 
@@ -61,69 +58,17 @@
     mkdir: {
       dev: {
         options: {
-          create: ["images"]
+          create: ['images']
         },
       },
     },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: "images_src/fixed/*.{gif,jpg,png}",
-          dest: "images/"
-        }]
-      },
-    },
-
-    /* Use PageSpeed Insights to check performance
-      - don't add to default as want to only run it independently
-      - PSI doesn"t work for me online as the page is only hosted locally */
-      pagespeed: {
-        options: {
-          nokey: true,
-          locale: "en_GB",
-          /* url: "http://192.168.0.18:8000/" Normally an external url*/
-          threshold: 80
-        },
-        local: {
-          options: {
-            /* url: "http://192.168.0.18:8000/" Normally an external url*/
-            strategy: "desktop",
-          }
-        },
-        mobile: {
-          options: {
-            strategy: "desktop",
-          }
-        }
-      }
-    });
-
-grunt.loadNpmTasks("grunt-responsive-images");
-grunt.loadNpmTasks("grunt-contrib-clean");
-grunt.loadNpmTasks("grunt-contrib-copy");
-grunt.loadNpmTasks("grunt-mkdir");
-grunt.registerTask("default", ["clean", "mkdir", "copy", "responsive_images"]);
-
-grunt.loadNpmTasks("grunt-pagespeed");
-
-// Register customer task for ngrok
-// Cannot get this to work :( ngrok insists on paid account for custome url
-  grunt.registerTask("psi-ngrok", "Run pagespeed with ngrok", function() {
-    var done = this.async();
-    var port = 8080;
-
-    ngrok.connect(port, function(err, url) {
-      if (err !== null) {
-        grunt.fail.fatal(err);
-        return done();
-      }
-      grunt.config.set("pagespeed.options.url", url);
-      grunt.task.run("pagespeed");
-      done();
-    });
   });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
 
 };
